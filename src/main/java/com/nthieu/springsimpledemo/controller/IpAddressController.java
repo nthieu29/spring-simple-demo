e89daeb60df918c4599c927a4afb049513a12ad5
nthieu29 <ngotrunghieu@live.com>
2021-07-25 14:22:24 +0800

    Add log for controller

--- src/main/java/com/nthieu/springsimpledemo/controller/IpAddressController.java
+++ src/main/java/com/nthieu/springsimpledemo/controller/IpAddressController.java
@@ -1,5 +1,6 @@
 package com.nthieu.springsimpledemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("ip-addresses")
+@Slf4j
 public class IpAddressController {
 
     @GetMapping
     public String getIpAddressInfo(HttpServletRequest request) {
+        log.info("New Request comes: " + request);
         String ipAddress = request.getHeader("X-Forward-For");
         if (ipAddress == null) {
             ipAddress = request.getRemoteAddr();
