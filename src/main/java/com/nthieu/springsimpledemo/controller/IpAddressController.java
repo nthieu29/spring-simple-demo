78789230f42c98e14719def513c229f4650240d9
nthieu29 <ngotrunghieu@live.com>
2021-07-25 19:34:49 +0800

    update ipAddress to request param

--- src/main/java/com/nthieu/springsimpledemo/controller/IpAddressController.java
+++ src/main/java/com/nthieu/springsimpledemo/controller/IpAddressController.java
@@ -4,8 +4,8 @@ import com.nthieu.springsimpledemo.service.IpAddressService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class IpAddressController {
     private final IpAddressService ipAddressService;
 
-    @GetMapping("/{ipAddress}")
-    public String getIpAddressInfo(@PathVariable String ipAddress) {
+    @GetMapping
+    public String getIpAddressInfo(@RequestParam String ipAddress) {
         return ipAddressService.getInfoForIpAddress(ipAddress);
     }
 }
